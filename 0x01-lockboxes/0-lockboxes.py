54f34ce8eec1e04b944f366f27cb73eb61583fff
justxd22 <xdjust18@gmail.com>
2024-07-04 23:13:51 +0300

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,18 +4,18 @@
 
 def canUnlockAll(boxes):
     """Lockboxes problem."""
-    keys = []
     opened = [False] * len(boxes)
+    opened[0] = True
     run = 2
 
     while run:
-        opened[0] = True
         for i in range(len(boxes)):
             if not opened[i]:
                 continue
             keys = boxes[i]
             for x in keys:
-                opened[x] = True
+                if x < len(boxes):
+                    opened[x] = True
         run -= 1
 
     return all(opened)
