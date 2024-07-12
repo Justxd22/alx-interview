4850ecbce8187625dc6771d9da9e49b27a242b7b
justxd22 <xdjust18@gmail.com>
2024-07-12 03:45:06 +0300

    init :D

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write H to x file with miniumm operations."""
+
+
+def minOperations(n):
+    """Write H to x file with miniumm operations."""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
