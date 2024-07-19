896bc4d8ff29e8cabe47cb723d56d653c40f55d7
justxd22 <xdjust18@gmail.com>
2024-07-19 03:30:04 +0300

    iinit :D

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""."""
+
+
+import sys
+
+
+codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+stats = {k: 0 for k in codes}
+x = 0
+f = 0
+
+def print_stats(stats: dict, file_size: int) -> None:
+    """."""
+    print(f"File size: {file_size}")
+    for k, v in sorted(stats.items()):
+        if v:
+            print(f"{k}: {v}")
+
+try:
+    for line in sys.stdin:
+        x += 1
+        data = line.split()
+        try:
+            code = data[-2]
+            if code in stats:
+                stats[code] += 1
+        except BaseException:
+            pass
+        try:
+            f += int(data[-1])
+        except BaseException:
+            pass
+        if x == 10:
+            x = 0
+            print_stats(stats, f)
+    print_stats(stats, f)
+except KeyboardInterrupt:
+    print_stats(stats, f)
+    raise
