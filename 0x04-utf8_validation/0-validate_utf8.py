cab0af28e0c77b2e3104041601e86bd4cda99f84
justxd22 <xdjust18@gmail.com>
2024-07-25 17:01:57 +0300

    init :D

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""UTF8 Validation."""
+
+
+def validUTF8(data):
+    """UTF8 Validation."""
+    blocks = 0
+
+    for num in data:
+        bina = format(num, '08b')
+
+        if blocks == 0:
+            if bina[0] == '0':
+                continue  # Single byte character (0xxxxxxx)
+            elif bina[:3] == '110':
+                blocks = 1  # 2-byte character (110xxxxx 10xxxxxx)
+            elif bina[:4] == '1110':
+                blocks = 2  # 3-byte character (1110xxxx 10xxxxxx 10xxxxxx)
+            elif bina[:5] == '11110':
+                blocks = 3  # 4-byte character
+            else:
+                return False
+        else:
+            # Check if the byte is a continuation byte (10xxxxxx)
+            if bina[:2] != '10':
+                return False
+            blocks -= 1
+
+    return blocks == 0
