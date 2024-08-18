933e69bef360c61279756f12e30945661e741f5e
justxd22 <xdjust18@gmail.com>
2024-08-18 23:33:03 +0300

    init :D

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rotate 2d matrix."""
+import os
+
+
+def rotate_2d_matrix(mat):
+    """Rotate 2d matrix."""
+    x = len(mat)
+    for xx in range(x):
+        for xxx in range(xx, x):
+            mat[xx][xxx], mat[xxx][xx] = mat[xxx][xx], mat[xx][xxx]
+
+    for xx in range(x):
+        mat[xx].reverse()
